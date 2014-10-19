026a0ea41c70d01b776ddf980dcecfdb67aacf1c
jeremiah sypult <jeremiah@sypult.com>
2014-10-19 03:20:23 -0500

    Cleaned up

--- BlissEmu/sys/windows/BlissCGC.h
+++
@@ -1,9 +0,0 @@
-
-#if defined( CLASSIC_GAME_CONTROLLER )
-extern bool usingCGC;
-void initCGC();
-void releaseCGC();
-
-typedef int (* CGCGETDATA)(UCHAR, USHORT *);
-extern CGCGETDATA L_CGCGetCookedIntyData;
-#endif
--- BlissEmu/sys/windows/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// Bliss.Mfc.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
